e469888e90005cabb96996343646024179d6634e
colleowino <colleowino@gmail.com>
2020-09-05 01:43:52 +0300

    script to copy libffmpeg from downloads to add the opera browsers i have

---
+++ scripts/opera-vids.sh
@@ -0,0 +1,3 @@
+sudo cp $HOME/Downloads/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera-developer/.
+sudo cp $HOME/Downloads/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/.
+sudo cp $HOME/Downloads/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera-beta/.
